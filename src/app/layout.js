7e1c14ed1ee6110741842478eb1fc68973ba9ee1
sana <sana.fathi30@gmail.com>
2024-10-08 00:12:38 +0330

    real estate project

---
+++ src/app/layout.js
@@ -0,0 +1,22 @@
+import NextAuthProvider from "@/providers/NextAuthProvider";
+import Layout from "@/layout/Layout";
+import { yekan } from "@/utils/fonts";
+import "./globals.css";
+
+export const metadata = {
+  title: "املاک",
+  description: "سایت خرید و فروش املاک",
+  icons: { icon: "./favicon.ico" },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body className={yekan.className}>
+        <NextAuthProvider>
+          <Layout>{children}</Layout>
+        </NextAuthProvider>
+      </body>
+    </html>
+  );
+}
